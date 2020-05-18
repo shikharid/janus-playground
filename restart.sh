bf4298087a5bc25baa12a47b1f86872fae096532
Shikhar <shikharid@gmail.com>
2020-05-18 20:07:54 +0530

    * fixes container name to pappu-janus to simplify restarts

--- restart.sh
+++ restart.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 sudo docker build -t shikharid/janus-pg .
-sudo docker stop -t 10 $(sudo docker ps -q --filter ancestor=shikharid/janus-pg)
-sudo docker run -d -p 7088:7088 -p 7889:7889 -p 8088:8088 -p 8089:8089 -p 1443:1443 -p 12000-13000:12000-13000/udp shikharid/janus-pg
+# stop container
+sudo docker stop -t 10 pappu-janus
+# clear container files
+sudo docker rm $(sudo docker ps -a -q)
+# run janus
+sudo docker run -d --name=pappu-janus -p 7088:7088 -p 7889:7889 -p 8088:8088 -p 8089:8089 -p 1443:1443 -p 12000-13000:12000-13000/udp shikharid/janus-pg
