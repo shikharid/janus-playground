7511dadebbf554256c3556aa995741b9628a858a
Shikhar <shikharid@gmail.com>
2020-05-18 20:40:38 +0530

    * save logs across restarts

--- restart.sh
+++ restart.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+CONTAINER=pappu-janus
 sudo docker build -t shikharid/janus-pg .
-# stop container
-sudo docker stop -t 10 pappu-janus
-# clear container files
+echo "janus image built"
+
+LOGFILE=/logs/janus/janus-`date +"%d-%m-%Y-%s"`
+sudo mkdir -p /logs/janus
+sudo docker cp $CONTAINER:/opt/janus/janus_server.log $LOGFILE
+
+sudo docker stop -t 10 $CONTAINER
+echo "active Janus container named ${CONTAINER} stopped, find logs at ${LOGFILE}"
+
 sudo docker rm $(sudo docker ps -a -q)
-# run janus
+echo "cleaned up stopped containers"
+
 sudo docker run -d --name=pappu-janus -p 7088:7088 -p 7889:7889 -p 8088:8088 -p 8089:8089 -p 1443:1443 -p 12000-13000:12000-13000/udp shikharid/janus-pg
+echo "janus started as docker container named ${CONTAINER}"
